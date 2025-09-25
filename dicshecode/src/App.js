a8d127c21c9e867cf28a4a1a1b5406b36c01fd66
Khin Myat Mon <58884581+KMyatMon@users.noreply.github.com>
2025-09-25 22:44:29 +0630

    dictionary app
    code update

--- dicshecode/src/App.js
+++ dicshecode/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="lily" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by KMM</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
